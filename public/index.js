5056a39991b8a61862d64ef11ca335d3608fd11a
Jared Capaldi <jcapaldi@uoguelph.ca>
2019-03-19 17:25:20 -0400

    Main Page 50% done

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 // Backend Call
 $(document).ready(function() {
         load_main_table();
+        $('#salaryTable').DataTable();
         // search_for_individuals_option("Samuel");
 });
 
@@ -21,13 +22,30 @@ function insert_into_website_table(person_array)
 {
     console.log("iNSERT INTO THE TABLE"); 
     console.log(person_array);
-    var person = person_array;
+    let person = person_array;
+    let table = document.getElementById('salaryTable');;
     for (i in person_array) {
-        var person = document.getElementById('table');
-        console.log(person)
-        person.innerHTML = person_array[i].First;
+        let btm = document.createElement("input");
+        btm.setAttribute('type','checkbox');
+        btm.onclick = selectedRow;
+        let row = table.insertRow(-1);
+        row.insertCell(0).appendChild(btm);
+        row.insertCell(1).innerHTML = person[i].FirstName +" "+ person[i].LastName;
+        row.insertCell(2).innerHTML = person[i].Sector;
+        if(person[i]['Salary Paid'] === undefined) {
+            row.insertCell(3).innerHTML = person[i].SalaryPaid;  
+        }
+        else {
+            row.insertCell(3).innerHTML = person[i]['Salary Paid'];
+        }
+        row.insertCell(4).innerHTML = "Province"
+        row.insertCell(5).innerHTML = person[i]['Calendar Year']
+        //person.innerHTML = person_array[i].First;
     }  
 } 
+function selectedRow() {
+    console.log("lol");
+}
 
 // function search_for_individuals_option(FirstName) 
 // {
