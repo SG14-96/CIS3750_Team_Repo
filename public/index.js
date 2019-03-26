a1671a5cd7b166f222506b27ebcd84348bb647f4
Jared Capaldi <jcapaldi@uoguelph.ca>
2019-03-25 20:54:44 -0400

    Bug fixing

--- public/index.js
+++ public/index.js
@@ -169,6 +169,7 @@ function paging(newPage) {
                     i++;
                 }
                 currGroup = tempGroup;
+                salaryGroup = currGroup;
                 newTbody(currSize,currGroup);
             },        
         });
